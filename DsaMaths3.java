2048d92b32f2b4b69f9fda9dfd353325c120f21c
Ashmita Chatterjee <iashmitac12@gmail.com>
2025-09-07 14:50:59 +0530

    Add files via upload

---
+++ DsaMaths3.java
@@ -0,0 +1,23 @@
+public class DsaMaths3 {
+
+    static boolean palindrome(int n) {
+        int revNum = 0;
+        int dup = n;
+        while (n > 0) {
+            int ld = n % 10;
+            revNum = (revNum * 10) + ld;
+            n = n / 10;
+        }
+        return dup == revNum;
+    }
+
+    public static void main(String[] args) {
+        int number = 49094;
+
+        if (palindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+    }
+}
