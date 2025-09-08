bee69086e4b295d146b6383766d28de56d598e14
Ashmita Chatterjee <iashmitac12@gmail.com>
2025-09-08 21:49:25 +0530

    Add files via upload

---
+++ DsaMaths4.java
@@ -0,0 +1,21 @@
+public class DsaMaths4 {
+    public static int findGcd(int a, int b) {
+        while (a > 0 && b > 0) {
+            if (a > b) {
+                a = a % b;
+            } else {
+                b = b % a;
+            }
+        }
+        if (a == 0) {
+            return b;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int n1 = 20, n2 = 15;
+        int gcd = findGcd(n1, n2);
+        System.out.println("GCD of " + n1 + " and " + n2 + " is: " + gcd);
+    }
+}
