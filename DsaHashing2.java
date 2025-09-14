03aa9678b253f146ac189b6d440414c359450c9d
Ashmita Chatterjee <iashmitac12@gmail.com>
2025-09-14 14:47:00 +0530

    Add files via upload

---
+++ DsaHashing2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class DsaHashing2 {
+
+    public static void main(String args[]) {  
+        int arr[] = {10, 5, 10, 15, 10, 5};
+        int n = arr.length;
+        countFreq(arr, n);  
+    }
+
+    public static void countFreq(int arr[], int n) {
+        boolean visited[] = new boolean[n];
+ 
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == true)
+                continue;
+
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+            System.out.println(arr[i] + " -> " + count);
+        }
+    }
+}
